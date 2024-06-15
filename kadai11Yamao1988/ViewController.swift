8de8d2e8e9462cfc0088013756f49f154e9ccb93
Tsuyoshi Kamada <ms.2bpd.cch@gmail.com>
2024-06-15 18:41:24 +0900

    REVISD

--- kadai11Yamao1988/ViewController.swift
+++ kadai11Yamao1988/ViewController.swift
@@ -9,30 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
     @IBOutlet weak var prefectureLabel: UILabel!
-    
-    
     
     @IBAction func change(_ sender: Any) {
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func exitFromTableCelltap(segue:UIStoryboardSegue) {
-        if let tableViewController = segue.source as? PrefectureTableViewController {
-            if let indexPath = tableViewController.tableView.indexPathForSelectedRow {
-                self.prefectureLabel.text = tableViewController.names[indexPath.row]
-            }
+    @IBAction func exitFromTableCelltap(segue: UIStoryboardSegue) {
+        if let tableViewController = segue.source as? PrefectureTableViewController,
+           let indexPath = tableViewController.tableView.indexPathForSelectedRow {
+            
+            self.prefectureLabel.text = tableViewController.names[indexPath.row]
         }
     }
     
     @IBAction func exitFromTableByCancell(segue:UIStoryboardSegue) {
     }
-    
 }
-
